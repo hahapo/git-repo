84a260a87b842425dab2a15fddbb8fd2db6403ed
hahapo <hahapo@protonmail.com>
2018-09-05 15:14:03 +0300

    task 5

--- Main.java
+++ Main.java
@@ -2,10 +2,35 @@ package ru.geekbrains.JL1HW;
 
 public class Main {
 
-    public static void main(String[] args) {
-	byte a = 1;
-	short b = 2;
-	int c = 3;
-	
-    }
+	public static void main(String[] args) {
+
+		System.out.println("Результат выражения (Task3) = " + task3(10, 5, 14, 7));
+		System.out.println("Результат выражения (Task4)" + task4(5,6));
+		System.out.println("Результат выражения (Task5)" + task5(-30));
+
+	}
+
+	public static void task2(String[] args){
+		byte bt = 1; 									//Создание ​​переменных и инициализация ​​всех ​​пройденных ​т​ипов ​​данных
+		short sh = 2;
+		int v = 3500000;
+		long l = 1500L;
+		float f = 13.5f;
+		double db = 193841;
+		boolean bool = true;
+		char ch = 'g';
+	}
+
+
+    public static int task3(int a, int b, int c, int d) {
+    	return a * (b + (c / d)); 						//Метод ​​вычисляющий ​​выражение ​​a​*​​(​b​​+​(​c​/​​​d))​​и ​​возвращающий ​​результат
+    	}
+
+	 public static boolean task4(int x1, int x2) {
+		 boolean bool;									//Метод принимающий на вход два числа, и проверяющий что их
+		 return ((x1 + x2) >= 10 && (x1 + x2) <= 20); 	//​​сумма лежит в пределах от​​ 10 ​​до ​​20(включительно)
+		}
+	 public static void  
 }
+
+
